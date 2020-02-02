09dbff14a762d73e8ce03b989fd0ee9093988bc9
yamsok <elouazza@etud.insa-toulouse.fr>
2020-02-02 17:27:25 +0100

    Selec d'une partie des 2 bandes et subplot

--- tests.py
+++ tests.py
@@ -9,11 +9,26 @@ from osgeo import gdal
 import numpy as np
 import rasterio
 from rasterio.plot import show
+import matplotlib.pyplot as plt
+### From stackoverflow, to make imshow subplot with individuals colorbars
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import LogNorm
+from matplotlib.ticker import MultipleLocator
+####
 
 filepath = r"../Data/img_tif.tif"
 raster = rasterio.open(filepath)
 
-show((raster,2))
+#show((raster, 1), cmap='Reds')
+#show((raster, 2), cmap='inferno')
+
+fig, (ax1, ax2) = plt.subplots(1,2)
+
+
+im1 = ax1.imshow(raster.read(2)[:400,800:])
+im2 = ax2.imshow(raster.read(1)[:400,800:])
+plt.show()
+
 ##
 ##raster = gdal.Open(filepath)
 ##
