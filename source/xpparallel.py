904bd8034146355001531bd88057309eb7ce4f35
El Ouazzani Mikael <elouazza@srv-gmmcalcul4>
2020-03-30 18:29:40 +0200

    new rez

--- source/xpparallel.py
+++ source/xpparallel.py
@@ -275,7 +275,7 @@ axis0 = 15 # décalage horizontal vers la gauche
 axis1 = 15  # décalage vertical vers le bas
 seuil = 15 # Seuil de norme pour les vecteur déplacements en px (rouge si > , magenta si <)
 bs = 256 # Bloc size
-f = 2 # Facteur de recouvrement
+f = 6 # Facteur de recouvrement
 #r = 25 # norme maximale en pixel admise pour le vecteur déplacement
     #
     # ### Partie Visualisation ###
