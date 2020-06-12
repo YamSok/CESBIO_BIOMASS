3829e82eab0a58ed2104a3491ee802714c4aa04e
YamSok <mika.elouazz@hotmail.fr>
2020-06-12 18:28:14 +0200

    encore du rangement

--- source/render_vector_field.py
+++ source/render_vector_field.py
@@ -6,21 +6,12 @@ Created on Sat Mar 28 17:03:26 2020
 @author: yamsok
 """
 
-# import os
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.axes_grid1 import make_axes_locatable
-# from matplotlib.colors import LogNorm
-# from matplotlib.ticker import MultipleLocator
-# from matplotlib.pyplot import figure
-# import matplotlib.patches as patches
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 100
 import warnings
 warnings.filterwarnings("ignore")
 
 from croscor import *
-
 
 c = choiceSimple()
 ff = ExtractFeatures(c)
